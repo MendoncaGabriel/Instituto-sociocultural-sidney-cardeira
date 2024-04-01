dfff31daab58b3fc6084a6c1c958e5f571e3d439
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-04-01 11:28:20 -0400

    add

--- src/models/database.js
+++ src/models/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_URL || `mongodb+srv://gabrielandrade05081997:2gnHgBzAXjNeM3XJ@cluster0.baicaa5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 module.exports = mongoose.connect(DATABASE_URL)
 .then(()=>{
